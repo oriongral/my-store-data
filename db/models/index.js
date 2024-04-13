6845683c1c07debac2a6c894d1009841bc4a3968
oriongral <percuafmx@hotmail.com>
2024-04-12 21:47:47 -0600

    Creando el modelo de la tabla user, index para mapear

---
+++ db/models/index.js
@@ -0,0 +1,8 @@
+const { func } = require('joi');
+const { User, UserSchema } = require('./user.model');
+
+function setupModels(sequelize) {
+  User.init(UserSchema, User.config(sequelize));
+}
+
+module.exports = setupModels;
